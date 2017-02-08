42263de6a99747c7e9f72799e682d7a97c231dc3
4015d3 <4015d3@acerola.ece.uprm.edu>
2017-02-08 15:14:05 -0400

    Created one inside and outside border.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -35,7 +35,7 @@ public class MyPanelClass extends JPanel {
                         
                       
                        
-                        
+                       
                         
                         
                         
